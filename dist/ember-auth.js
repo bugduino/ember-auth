b3a5fb118f189555122d9f7b34a1e068638552f3
giulyquinto <onlyforwin@gmail.com>
2014-01-09 14:24:30 +0100

    returned to old version, added the removehandler function to the signIn action

--- dist/ember-auth.js
+++ dist/ember-auth.js
@@ -415,36 +415,37 @@ set$(get$(Em, 'Auth'), 'SignInController', get$(Em, 'Controller').extend({
   password: null,
   error: null,
   actions: {
-    successHandler: function (response) {
-      var accessToken;
-      console.log('success signIn');
-      this.set('error', null);
-      this.set('username', '');
-      this.set('password', '');
-      accessToken = get$(this, 'auth').get('authToken');
-      if (accessToken) {
-        get$(this, 'auth').createSession(JSON.stringify({ access_token: accessToken }));
-        return localStorage.setItem('access_token', accessToken);
-      }
-    },
-    errorHandler: function (error) {
-      console.log('fail signIn');
-      return this.set('error', get$(error, 'error_description'));
-    },
     signIn: function () {
-      var clientId, data, password, username;
+      var clientId, password, this$, this$1, username;
       username = this.get('username');
       password = this.get('password');
       clientId = get$(TreggEditor, 'clientId');
-      data = {
-        client_id: clientId,
-        username: username,
-        password: password,
-        grant_type: 'password'
-      };
-      get$(this, 'auth').signIn(data);
-      get$(this, 'auth').addHandler('signInSuccess', this.send('successHandler'));
-      return get$(this, 'auth').addHandler('signInError', this.send('errorHandler'));
+      get$(this, 'auth').signIn({
+        data: {
+          client_id: clientId,
+          username: username,
+          password: password,
+          grant_type: 'password'
+        }
+      });
+      get$(this, 'auth').addHandler('signInSuccess', (this$ = this, function (response) {
+        var accessToken;
+        console.log('success signIn');
+        this$.set('error', null);
+        this$.set('username', '');
+        this$.set('password', '');
+        accessToken = get$(this$, 'auth').get('authToken');
+        if (accessToken) {
+          get$(this$, 'auth').createSession(JSON.stringify({ access_token: accessToken }));
+          return localStorage.setItem('access_token', accessToken);
+        }
+      }));
+      get$(this, 'auth').addHandler('signInError', (this$1 = this, function (error) {
+        console.log('fail signIn');
+        return this$1.set('error', get$(error, 'error_description'));
+      }));
+      get$(this, 'auth').removeHandler('signInSuccess');
+      return get$(this, 'auth').removeHandler('signInError');
     },
     signUp: function () {
       var clientId, confirmPassword, data, password, signUpUrl, this$, this$1, username;
